d92de3cd81f44a69afb87b78e9c4453f09ee00e1
André Oliveira <andreoliveira208@gmail.com>
2023-05-20 10:24:56 +0100

    Update HTBCalculator.py

--- PyScripts/HTBCalculator.py
+++ PyScripts/HTBCalculator.py
@@ -1,45 +1,9 @@
-def CalcHTBRank(ActiveSystemOwns, ActiveUserOwns , ActiveChallengeOwns, activeMachines, activeChallenges):
-    """Part1 = (ActiveSystemOwns + (ActiveUserOwns / 2) + (ActiveChallengeOwns / 10))
-    Part2 = ((activeMachines + (activeMachines / 2) + (activeChallenges / 10)) * 100)
-    if((Part1 <= 0 and Part2 <= 0) or (Part2 <= 0)):
-        Rankis = 0
-    else:
-        Rankis = (Part1 / Part2)
-    if(Rankis > 100):
-        Rankis = 100
-    elif(Rankis < 0):
-        Rankis = 0
-    """
-    Rankis = (ActiveSystemOwns + (ActiveUserOwns / 2) + (ActiveChallengeOwns / 10)) / (activeMachines + (activeMachines / 2) + (activeChallenges / 10)) * 100
-    return Rankis
+from decimal import Decimal
+ActiveSystemOwns = Decimal(input('How many Root owns you did? '))
+ActiveUserOwns = Decimal(input('How many User owns you did? '))
+ActiveChallengeOwns = Decimal(input('How many Challenges you completed (ChallengeOwns)? '))
+activeMachines = Decimal(input('How many active Machines exist? '))
+activeChallenges = Decimal(input('How many active Challenges exist? '))
 
-print("-*-"*20)
-print(" "*17+"HackTheBox Calculator")
-print("-*-"*20)
-print("$ ActiveSystemOwns:  ")
-ActiveSOwns = int(input())
-print("$ ActiveUserOwns:  ")
-ActiveUOwns = int(input())
-print("$ ActiveChallengeOwns:  ")
-ActiveCOwns = int(input())
-print("$ activeMachines:  ")
-ActiveM = int(input())
-print("$ activeChallenges:  ")
-ActiveC = int(input())
-Rank = CalcHTBRank(ActiveSOwns, ActiveUOwns, ActiveCOwns, ActiveM, ActiveC )
-SRank= str(Rank)
-if(Rank <= 5):
-    print("Noob Rank: "+ SRank+ "%")
-elif(Rank <= 20):
-    print("Script Kiddie Rank: "+ SRank + "%")
-elif(Rank <= 45):
-    print("Hacker Rank: "+ SRank + "%")
-elif(Rank <= 70):
-    print("Pro Hacker Rank: "+ SRank + "%")
-elif(Rank <= 90):
-    print("Elite Hacker Rank: "+ SRank + "%")
-elif(Rank < 100):
-    print("Guru Rank: "+ SRank+ "%")
-elif(Rank == 100):
-    print("Omniscient Rank: "+ SRank + "%")
- 
+Points = (ActiveSystemOwns + (ActiveUserOwns / 2) + (ActiveChallengeOwns / 10)) / (activeMachines + (activeMachines / 2) + (activeChallenges / 10)) * 100
+print(f"{Points}%")
