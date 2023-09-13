3a0c9290454375e65545727bb32de6f4f08717b4
André Oliveira <andreoliveira208@gmail.com>
2023-09-13 12:59:11 +0100

    Update installJDK15.sh

--- toolsInstaller/installJDK15.sh
+++ toolsInstaller/installJDK15.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Created by WafflesExploit
 # Downloads Java 15 and extract it to /usr/lib/jvm/jdk-15.0.2
 cd ~/Downloads
 wget -O openjdk-15.0.2_linux-x64_bin.tar.gz 'https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-x64_bin.tar.gz'
