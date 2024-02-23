6e468e2335e5c87efee4123dfb1c89200ce3942a
André Oliveira <andreoliveira208@gmail.com>
2024-02-23 18:45:19 +0000

    Delete UtilScripts directory

--- UtilScripts/ttf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo '------Text To File------'
-if [[ -z "$1" ]] || [[ -z "$2" ]]; then
-	echo '$ ttf.sh <text> <file>'
-	echo 'Creates/Overwrites text to a file.' 
-	exit 0
-fi
-echo "$1" > $2
-echo "$2 Has been created."
