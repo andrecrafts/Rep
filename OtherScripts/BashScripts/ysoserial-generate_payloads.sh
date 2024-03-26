288951ba6fe2dbfd16dcd983da7cf0ad820f347f
André Oliveira <andreoliveira208@gmail.com>
2024-03-26 09:33:23 +0000

    Update ysoserial-generate_payloads.sh

--- OtherScripts/BashScripts/ysoserial-generate_payloads.sh
+++ OtherScripts/BashScripts/ysoserial-generate_payloads.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-# --Generate all payloads--
 command="$@"
 if [ -z "$command" ]; then
-    echo "./generate_payloads.sh <commands>"
-    exit
+	echo "./generate_payloads.sh <commands>"
+	exit
+fi
+if [ -f "serialized_payloads" ]; then
+	echo "Do you want to delete the old payloads? [n/y]"
+	read -r ans
+	if [[ "$ans" == "y" || "$ans" == "Y" ]]; then
+		rm serialized_payloads
+		echo "deleted"
+	fi
 fi
 payload_file="$(pwd)/payloads.txt"
 while read -r payload; do
