eef6903216dbf943b34755dbf1e0d355720dcb6e
André Oliveira <andreoliveira208@gmail.com>
2024-04-02 16:49:20 +0100

    Update Exfil-via-base64.ps1

--- OtherScripts/PowerShell/Exfil-via-base64.ps1
+++ OtherScripts/PowerShell/Exfil-via-base64.ps1
@@ -1,13 +1,26 @@
-function encode_b64 ($file_path, $newfile_path) {
-    $content = Get-Content -Path $file_path -Raw
-    $byte_array = [System.Text.Encoding]::UTF8.GetBytes($content)
-    $base64 = [System.Convert]::ToBase64String($byte_array)
-	  $base64 > $newfile_path
-	  Write-Host "Encoded base64 to $newfile_path"
+# Made by WafflesExploits
+# > Encode any type of file, and decoded as text or as a file.
+
+function encode_b64 ([Parameter(Mandatory)] [string] $file_path, [Parameter(Mandatory)] [string] $newfile_path) {
+	$byte_array = [System.IO.File]::ReadAllBytes($file_path)
+	$base64 = [System.Convert]::ToBase64String($byte_array)
+	$base64 > $newfile_path
+	Write-Host "Encoded Base64 to $newfile_path"
 }
-function decode_b64 ($file_path, $newfile_path) {
+function b64_totext ([Parameter(Mandatory)] [string] $file_path, [Parameter(Mandatory)] [string] $newfile_path) {
 	$content = Get-Content -Path $file_path -Raw
 	$decodedbase64 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($content))
 	$decodedbase64 > $newfile_path
-	Write-Host "Decoded base64 to $newfile_path"
+	Write-Host "Decoded Base64 to $newfile_path"
+}
+function b64_tofile ([Parameter(Mandatory)] [string] $file_path, [Parameter(Mandatory)] [string] $newfile_path) {
+	$base64String = Get-Content -Path $file_path -Raw
+	$fileBytes = [System.Convert]::FromBase64String($base64String)
+	[System.IO.File]::WriteAllBytes($newfile_path, $fileBytes)
+	Write-Host "Created $newfile_path from Base64"
+}
+function list_commands () {
+ 	Write-Host "> encode_b64 <file_path> <newfile_path>"
+	Write-Host "> b64_totext <file_path> <newfile_path>"
+	Write-Host "> b64_tofile <file_path> <newfile_path>"
 }
